0efc121ba3042ab58d0ef596409b0a1ece39209b
brendonb <brendon.barends@gmail.com>
2014-07-31 19:07:16 +0159

    First commit

---
+++ kata3MyTwitterApplication.java
@@ -0,0 +1,36 @@
+package za.co.jozijugdojo;
+
+import java.util.List;
+import java.util.Scanner;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+
+public class kata3MyTwitterApplication {
+	 public static void main(String[] args) {
+	
+	System.out.println("Please enter a twitter account name and press ENTER (without the @)");
+	
+	Scanner input = new Scanner(System.in);
+	String userInputString = input.nextLine();
+	System.out.println("You entered" +" "+ userInputString);
+//	Twitter twitter = new TwitterFactory().getInstance();
+	Twitter twitter = TwitterFactory.getSingleton();
+	
+	try{
+	User user = twitter.showUser(userInputString);
+	System.out.println(user.getFollowersCount());
+	
+	//My COde goes here
+	System.out.println(user.getStatus());
+	
+	}catch(TwitterException tExp) {
+	
+	System.out.println("Twitter error :" + tExp.getMessage());
+	}
+	}
+	}
+	
+		
